c9f5d2b813a27127c1ff4e18970d0c15c97b3088
tcJackClay <tcJackClay@example.com>
2026-03-10 21:13:50 +0800

    fix: 修复重复表头问题，CreateAssetNode移除名称输入

--- src/components/nodes/CreateAssetNode.tsx
+++ src/components/nodes/CreateAssetNode.tsx
@@ -176,6 +176,7 @@ export default function CreateAssetNode({ nodeId, data, updateData }: CreateAsse
         {/* 左侧：名称 + 类型/变体/父资产 */}
         <div className="flex-1 space-y-1">
           {/* 资产类型选择 - 仅在非变体时显示 */}
+          {/* 资产类型选择 - 仅在非变体时显示 */}
           {/* Name Input */}
           <input
             type="text"
